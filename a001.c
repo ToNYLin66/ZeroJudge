aed4601cbff0772a7c4ca19b5e2e80e01c069ace
Ting-Yu Lin <44372997+ToNYLin66@users.noreply.github.com>
2022-02-23 03:31:15 +0800

    Add files via upload

---
+++ a001.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void) {
+    char a[100];
+    scanf("%s", a);
+    printf("hello, %s\n", a);
+    return 0;
+}
---
+++ a001.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(void) {
+    char a[100];
+    cin >> a;
+    cout << "hello, " << a << endl;
+    return 0;
+}
